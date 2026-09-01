83a7bf1a86b1b0947a462e59f9ddd7c6452bc775
DOWNTOWNVILLA <downtownvillaofficial@gmail.com>
2026-09-01 11:38:01 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -64,11 +64,6 @@ def _create_model(instance):
             indexes = ("$file_name",)
             collection_name = COLLECTION_NAME
 
-        @classmethod
-        async def ensure_indexes(cls):
-            """Create the $file_name index if it doesn't exist."""
-            await cls.collection.create_index("$file_name")
-
     instance.register(MediaModel)
     return MediaModel
 
@@ -78,6 +73,7 @@ COLLECTIONS = []
 for db in _all_dbs:
     inst = Instance.from_db(db)
     model = _create_model(inst)
+    # Umongo sets model.collection automatically, but we assign explicitly to be safe
     model.collection = db[COLLECTION_NAME]
     MODELS.append(model)
     COLLECTIONS.append(db[COLLECTION_NAME])
